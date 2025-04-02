5af7366011bc7001a35a708c193924dcc50aac25
Cyrus Yiu <UnsignedArduino@outlook.com>
2025-04-02 01:03:13 -0400

    Utility functions

--- main.ts
+++ main.ts
@@ -1,1 +1,58 @@
-
+function distance_from_bottom_to_wall_below (s: Sprite) {
+    if (s.isHittingTile(CollisionDirection.Bottom)) {
+        return 0
+    }
+    local_loc = s.tilemapLocation()
+    while (!(tiles.tileAtLocationIsWall(local_loc))) {
+        local_loc = local_loc.getNeighboringLocation(CollisionDirection.Bottom)
+    }
+    return local_loc.top - s.bottom
+}
+function is_arrow_key_pressed () {
+    return controller.left.isPressed() || controller.up.isPressed() || (controller.right.isPressed() || controller.down.isPressed())
+}
+function location_sprite_hitting (s: Sprite) {
+    if (s.isHittingTile(CollisionDirection.Left)) {
+        return s.tilemapLocation().getNeighboringLocation(CollisionDirection.Left)
+    } else if (s.isHittingTile(CollisionDirection.Top)) {
+        return s.tilemapLocation().getNeighboringLocation(CollisionDirection.Top)
+    } else if (s.isHittingTile(CollisionDirection.Right)) {
+        return s.tilemapLocation().getNeighboringLocation(CollisionDirection.Right)
+    } else if (s.isHittingTile(CollisionDirection.Bottom)) {
+        return s.tilemapLocation().getNeighboringLocation(CollisionDirection.Bottom)
+    } else {
+        return s.tilemapLocation()
+    }
+}
+function sprite_in_water (s: Sprite) {
+    for (let tile of [assets.tile`myTile13`, assets.tile`myTile14`, assets.tile`myTile16`]) {
+        if (s.tileKindAt(TileDirection.Center, tile)) {
+            return true
+        }
+    }
+    return false
+}
+function is_sprite_hitting_wall (s: Sprite) {
+    return s.isHittingTile(CollisionDirection.Left) || s.isHittingTile(CollisionDirection.Top) || (s.isHittingTile(CollisionDirection.Right) || s.isHittingTile(CollisionDirection.Bottom))
+}
+function create_base_environment () {
+    scene.setBackgroundColor(9)
+    tiles.setCurrentTilemap(tilemap`level2`)
+}
+function unique_colors_in_tile (img2: Image) {
+    local_colors = []
+    for (let local_y = 0; local_y <= 15; local_y++) {
+        for (let local_x = 0; local_x <= 15; local_x++) {
+            if (!(img2.getPixel(local_x, local_y) == 0)) {
+                local_colors.push(img2.getPixel(local_x, local_y))
+            }
+        }
+    }
+    arrays.purge(local_colors)
+    return local_colors
+}
+let local_colors: number[] = []
+let local_loc: tiles.Location = null
+stats.turnStats(true)
+let SHOW_PLAYER_DEBUG = false
+create_base_environment()
--- tilemap.g.ts
+++ tilemap.g.ts
@@ -2,11 +2,17 @@
 namespace myTiles {
     //% fixedInstance jres blockIdentity=images._tile
     export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile15 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile18 = image.ofBuffer(hex``);
 
     helpers._registerFactory("tilemap", function(name: string) {
         switch(helpers.stringTrim(name)) {
-            case "level1":
-            case "level1":return tiles.createTilemap(hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`, img`
+            case "level2":
+            case "level2":return tiles.createTilemap(hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`, img`
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
@@ -23,7 +29,7 @@ namespace myTiles {
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
-`, [myTiles.transparency16], TileScale.Sixteen);
+`, [myTiles.transparency16,myTiles.tile15,myTiles.tile16,myTiles.tile18], TileScale.Sixteen);
         }
         return null;
     })
@@ -31,6 +37,12 @@ namespace myTiles {
     helpers._registerFactory("tile", function(name: string) {
         switch(helpers.stringTrim(name)) {
             case "transparency16":return transparency16;
+            case "myTile13":
+            case "tile15":return tile15;
+            case "myTile14":
+            case "tile16":return tile16;
+            case "myTile16":
+            case "tile18":return tile18;
         }
         return null;
     })
